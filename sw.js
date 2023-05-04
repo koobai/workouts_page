afff28685690edefd3125bb06c515d333aee166e
koobai <koobai@users.noreply.github.com>
2023-05-04 00:48:01 +0000

    deploy: koobai/workouts@e89683111e6f3a3eeca4825aaf3e498aa7b0477a

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-066662e40dc38748ef07.js"
+    "url": "webpack-runtime-aa70b7ff792773197e8e.js"
   },
   {
     "url": "framework-6613d4b2b9bb194e1193.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "866969b9857f01af3bc89bce6c22fe38"
+    "revision": "2122e3cb38a2099c5cea191b00c658df"
   },
   {
     "url": "polyfill-3d56fda09f0b7ce832de.js"
