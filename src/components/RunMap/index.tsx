bbd16ad21a7d8f9e3692110820b145c458646e90
koobai <mekoobai@gmail.com>
2026-02-24 19:19:41 +0800

    add

--- src/components/RunMap/index.tsx
+++ src/components/RunMap/index.tsx
@@ -189,7 +189,7 @@ const RunMap = ({
       {...viewState}
       onMove={onMove}
       style={style}
-      mapStyle="mapbox://styles/mapbox/navigation-night-v1"
+      mapStyle="mapbox://styles/mapbox/dark-v10"
       ref={mapRefCallback}
       mapboxAccessToken={MAPBOX_TOKEN}
       interactiveLayerIds={['runs2-hover-area']}
